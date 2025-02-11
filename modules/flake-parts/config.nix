c7838da3597a1970c799fb5412150a3cdc4adb9b
Roy Ren <royite.ren@gmail.com>
2025-02-11 13:43:07 +0800

    git init nixos

---
+++ modules/flake-parts/config.nix
@@ -0,0 +1,27 @@
+{ lib, ... }:
+{
+  options = {
+    me = lib.mkOption {
+      default = { };
+      type = lib.types.submodule {
+        options = {
+          username = lib.mkOption {
+            type = lib.types.str;
+            default = "roy";
+            description = "Your username as shown by `id -un`";
+          };
+          fullname = lib.mkOption {
+            type = lib.types.str;
+            default = "Roy Ren";
+            description = "Your full name for use in Git config";
+          };
+          email = lib.mkOption {
+            type = lib.types.str;
+            default = "johndoe@nixpad.com";
+            description = "Your email for use in Git config";
+          };
+        };
+      };
+    };
+  };
+}
